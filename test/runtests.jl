51a2e7f037a3b6f254843b7be50a8417a1bf469b
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2021-11-03 23:46:09 +0200

    one unit test completed

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,22 @@
 
 import Test
+import JLD
+import TextBlockExtractor
+import DataFrames
+
+data_dir = joinpath(dirname(pathof(TextBlockExtractor)), "..", "data", "examples") 
+example_1_result = TextBlockExtractor.extract_text_blocks(
+    joinpath(data_dir, "example_1_data.R") 
+)
+example_1_expected = JLD.load(
+    joinpath(data_dir, "example_1_expected.jld") , "example_1_expected"
+)
+for row_no = 1:DataFrames.nrow(example_1_expected)
+    for col_no = 1:DataFrames.ncol(example_1_expected)
+        Test.@test example_1_result[row_no, col_no] == example_1_expected[row_no, col_no]
+    end
+end
 
 
 
-Test.@test 1 == 1
 
